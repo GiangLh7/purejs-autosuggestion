b93102edb40b1d7f1b76f5c0e0ac4befee837089
Unknown <hoang_giang_bk@yahoo.com>
2018-04-20 01:25:50 +0700

    Add event handlers for input element and delegate event for suggestion item
    keydown, keyup, blur, focus

--- js/exam.js
+++ js/exam.js
@@ -15,11 +15,11 @@ class AutoSuggestion {
       /**
        * Minimum characters required to trigger the suggestion
        */
-      minChars: 1,
+      minChars: 0,
       /**
-       * The delay time to display the suggestion box
+       * The delay time to perform search suggestions
        */
-      delay: 500,
+      delay: 100,
       /**
        * Css class for the suggestion box
        */
@@ -31,6 +31,7 @@ class AutoSuggestion {
     };
     this.element = element; // input element to attach aut-suggestion function
     this.options = Object.assign({}, this.defaultOptions, options);
+    this.cache = {};
     this.initialize();
   }
   /**
@@ -41,6 +42,129 @@ class AutoSuggestion {
     this.suggestionContainer = document.createElement('div');
     this.suggestionContainer.className = `autocomplete-suggestions ${this.options.menuClass}`;
     document.body.appendChild(this.suggestionContainer);
+  
+    // register event listeners on the input element
+    Utils.addEvent(this.element, 'keydown', this.onInputKeyDown.bind(this));
+    Utils.addEvent(this.element, 'keyup', this.onInputKeyup.bind(this));
+    Utils.addEvent(this.element, 'blur', this.onInputBlur.bind(this));
+    if (!this.options.minChars) {
+      Utils.addEvent(this.element, 'focus', this.onInputFocus.bind(this));
+    }
+  
+    // delegate suggestion item's event
+    Utils.delegateEvent("autocomplete-suggestion", "mousedown", (selectedSuggestion) => {
+    
+    }, this.suggestionContainer);
+  
+    Utils.delegateEvent("autocomplete-suggestion", "mouseover", (selectedSuggestion) => {
+      const currentSelectedSuggestion = this.suggestionContainer.querySelector('.autocomplete-suggestion.selected');
+      if (currentSelectedSuggestion) {
+        Utils.removeClass(currentSelectedSuggestion, "selected");
+      }
+      Utils.addClass(selectedSuggestion, "selected");
+    }, this.suggestionContainer);
+  
+    Utils.delegateEvent("autocomplete-suggestion", "mouseleave", () => {
+      const selectedSuggestion = this.suggestionContainer.querySelector('.autocomplete-suggestion.selected');
+      if (selectedSuggestion) {
+        setTimeout(() => {
+          Utils.removeClass(selectedSuggestion, "selected");
+        }, 20);
+      }
+    }, this.suggestionContainer);
+  }
+  /**
+   * Handle focus event on input element
+   */
+  onInputFocus(e) {
+    this.lastSearchTerm = '\n';
+    this.onInputKeyup(e);
+  }
+  /**
+   * Handle keydown event on input element
+   */
+  onInputKeyDown(e) {
+    const key = e.keyCode;
+    // down (40), up (38)
+    if ((key === 40 || key === 38) && this.suggestionContainer.innerHTML) {
+      let nextSelectedSuggestion, selectedSuggestion = this.suggestionContainer.querySelector('.autocomplete-suggestion.selected');
+      if (!selectedSuggestion) {
+        // if there is no currently selected item, then move to the first or last suggestion item depending on key is down or up
+        nextSelectedSuggestion = (key === 40) ? this.suggestionContainer.querySelector('.autocomplete-suggestion') :
+          this.suggestionContainer.childNodes[this.suggestionContainer.childNodes.length - 1];
+        Utils.addClass(nextSelectedSuggestion, "selected");
+        this.element.value = nextSelectedSuggestion.getAttribute('data-val');
+      } else {
+        // move to the next or previous sibling
+        Utils.removeClass(selectedSuggestion, "selected");
+        nextSelectedSuggestion = (key === 40) ? selectedSuggestion.nextSibling : selectedSuggestion.previousSibling;
+        if (nextSelectedSuggestion) {
+          Utils.addClass(nextSelectedSuggestion, "selected");
+          this.element.value = nextSelectedSuggestion.getAttribute('data-val');
+        }
+        else {
+          this.element.value = this.lastSearchTerm;
+          nextSelectedSuggestion = 0;
+        }
+      }
+      this.updateSuggestionContainer(0, nextSelectedSuggestion);
+      return false;
+    }
+    // esc
+    else if (key === 27) {
+      this.element.value = this.lastSearchTerm;
+      Utils.debounce(this.hideSuggestion.bind(this), 20)();
+    }
+    // enter
+    else if (key === 13 || key === 9) {
+      e.preventDefault();
+      let selectedSuggestion = this.suggestionContainer.querySelector('.autocomplete-suggestion.selected');
+      if (selectedSuggestion && this.suggestionContainer.style.display !== 'none') {
+        this.onSelectSuggestion(selectedSuggestion.getAttribute('data-val'));
+        Utils.debounce(this.hideSuggestion.bind(this), 20)();
+      }
+    }
+  }
+  /**
+   * Handle keyup event on input element
+   */
+  onInputKeyup(e) {
+    const key = window.event ? e.keyCode : e.which;
+    if (!key || (key < 35 || key > 40) && key !== 13 && key !== 27) {
+      let searchTerm = this.element.value; // get the search term
+      if (searchTerm.length >= this.options.minChars) {
+        if (searchTerm !== this.lastSearchTerm) {
+          this.lastSearchTerm = searchTerm;
+          clearTimeout(this.timer);
+          if (this.options.cache) {
+            if (searchTerm in this.cache) { // if the search already cached
+              this.suggest(this.cache[searchTerm]);
+              return;
+            }
+            // check if previous suggestions, if there was no
+            for (let i=1; i<searchTerm.length-this.options.minChars; i++) {
+              let previousSearchTerm = searchTerm.slice(0, searchTerm.length-i);
+              if (previousSearchTerm in this.cache && !this.cache[previousSearchTerm].length) {
+                this.suggest([]); return;
+              }
+            }
+          }
+          // if the search term is not cached, then perform new search against data-source
+          this.timer = setTimeout(() => {
+            this.options.dataSource(searchTerm, this.suggest.bind(this));
+          }, this.options.delay);
+        }
+      } else {
+        this.lastSearchTerm = searchTerm;
+        Utils.debounce(this.hideSuggestion.bind(this), 20)();
+      }
+    }
+  }
+  /**
+   * Handle blur event on input element
+   */
+  onInputBlur() {
+    
   }
   /**
    * Responsible for building the suggestion list based on returned data from data source
@@ -72,8 +196,25 @@ class AutoSuggestion {
   /**
    * Update position and layout of suggestion container
    */
-  updateSuggestionContainer() {
-  
+  updateSuggestionContainer(resize, nextSuggestionItem) {
+    let rect = this.element.getBoundingClientRect();
+    this.suggestionContainer.style.left = Math.round(rect.left + (window.pageXOffset || document.documentElement.scrollLeft) + this.options.offsetLeft) + 'px';
+    this.suggestionContainer.style.top = Math.round(rect.bottom + (window.pageYOffset || document.documentElement.scrollTop) + this.options.offsetTop) + 'px';
+    this.suggestionContainer.style.width = Math.round(rect.right - rect.left) + 'px'; // outerWidth
+    if (!resize) {
+      this.suggestionContainer.style.display = 'block';
+      if (!this.suggestionContainer.maxHeight) { this.suggestionContainer.maxHeight = parseInt((window.getComputedStyle ? getComputedStyle(this.suggestionContainer, null) : this.suggestionContainer.currentStyle).maxHeight); }
+      if (!this.suggestionContainer.suggestionHeight) this.suggestionContainer.suggestionHeight = this.suggestionContainer.querySelector('.autocomplete-suggestion').offsetHeight;
+      if (this.suggestionContainer.suggestionHeight)
+        if (!nextSuggestionItem) this.suggestionContainer.scrollTop = 0;
+        else {
+          let scrTop = this.suggestionContainer.scrollTop, selTop = nextSuggestionItem.getBoundingClientRect().top - this.suggestionContainer.getBoundingClientRect().top;
+          if (selTop + this.suggestionContainer.suggestionHeight - this.suggestionContainer.maxHeight > 0)
+            this.suggestionContainer.scrollTop = selTop + this.suggestionContainer.suggestionHeight + scrTop - this.suggestionContainer.maxHeight;
+          else if (selTop < 0)
+            this.suggestionContainer.scrollTop = selTop + scrTop;
+        }
+    }
   }
   /**
    * Hide suggestion box
@@ -104,6 +245,80 @@ class AppsSuggestion extends AutoSuggestion{
   
   onSelectSuggestion(item) {
   
+  }
+}
+
+class Utils {
+  static removeClass(el, cssClass) {
+    el.className = el.className.replace(cssClass, '');
+  }
+  static addClass(el, cssClass) {
+    el.className += ` ${cssClass}`;
+  }
+  static debounce(fn, delay) {
+    let timer = null;
+    return function() {
+      let context = this;
+      let args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function(){
+        fn.apply(context, args);
+      }, delay);
+    }
+  }
+  /**
+   * register event listener
+   * @param {DOM} el - element to listen
+   * @param {string} type - event to listen
+   * @param {function} handler - handler when event occurs
+   */
+  static addEvent(el, type, handler) {
+    if (el.attachEvent) {
+      el.attachEvent(`on${type}`, handler);
+    } else {
+      el.addEventListener(type, handler);
+    }
+  }
+  /**
+   * unregister event listener
+   * @param {DOM} el - element to listen
+   * @param {string} type - event to listen
+   * @param {function} handler - handler when event occurs
+   */
+  static removeEvent(el, type, handler){
+    // if (el.removeEventListener) not working in IE11
+    if (el.detachEvent) {
+      el.detachEvent(`on${type}`, handler);
+    }
+    else {
+      el.removeEventListener(type, handler);
+    }
+  }
+  /**
+   * delegate event.
+   * Instead of add event listener for each element having a specific css class,
+   * just listen event on a wrapper element (context).
+   * When event is raised, we need to resolve the target element based its css class
+   * @param {string} elClass - css class to resolve element when the event raised
+   * @param {string} type - event to listen
+   * @param {function} cb - call back function when the event is for target element
+   * @param {DOM} context - the wrapper element to listen event on
+   */
+  static delegateEvent(elClass, type, cb, context) {
+    this.addEvent(context || document, type, function (e) {
+      let found, el = e.target || e.srcElement;
+      // loop to find the source element
+      while (el && !(found = Utils.hasClass(el, elClass))) {
+        el = el.parentElement;
+      }
+      if (found) cb(el, e);
+    });
+  }
+  /**
+   * Check if an element has a specific css class
+   */
+  static hasClass(el, className){
+    return el.classList ? el.classList.contains(className) : new RegExp('\\b'+ className+'\\b').test(el.className);
   }
 }
 
