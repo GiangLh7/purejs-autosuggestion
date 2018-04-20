4389c04cec37e022977569b78509d5a6de6ff1a2
EP\gile <giang.le@episerver.com>
2018-04-20 14:52:37 +0700

    Adding SASS style and webpack config to build css

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const SOURCE_PATH = path.resolve(__dirname, '.');
+
+module.exports = function () {
+  
+  const webpackConfig = {
+    entry: ['./css/main.scss'],
+    output: {
+      path: SOURCE_PATH
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].css',
+                outputPath: 'css/'
+              }
+            },
+            {
+              loader: 'extract-loader'
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        }
+      ]
+    }
+  };
+  
+  return webpackConfig;
+};
